9192bae493645106644489d366ecba53316a069a
cassidyrobles <cassidyrobles19@gmail.com>
2023-01-09 13:14:37 -0500

    fwgffhbff

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -9,4 +9,6 @@ console.log("end");
 //JavaScript can only run one function at a time and should go in the order of start, timer, and then end but because there is a 3 second delay end should not be able to run until timer runs after the three seconds
 //because the project is running in browser it will load start, then end, then times 3 seconds later, but it will not wait to log timer before it logs end
 //starts runs and works, timer runs and is pushed to the side because it needs three seconds, and end goes while timer is on the side then timer returns and finishes
-//
+//promises say that the line will be prevented from loading until the data loads
+//once a promise is fulfilled you can do something with it
+//sometimes data takes a long time to load and will go on to the next line of code, which is bad and promises will help prevent this
